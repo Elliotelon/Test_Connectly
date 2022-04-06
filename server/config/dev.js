e9d42b29d651480dfd40034b2a6515838df9dc51
kimminkyu <elliotelon@gmail.com>
2022-04-06 17:46:47 +0900

    서버

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //실제로는 깃헙에 올리지 않음 과제수행으로 인해 올림.
+    mongoURI : "mongodb+srv://elliot:PfQyEYZknoxDly7o@cluster0.cjy1b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
